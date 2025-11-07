1ef929254e703d1d8cf19e5c89b2221d53f5811b
JoaoYamaguti <joaov.yamaguti@gmail.com>
2025-11-06 23:28:40 -0300

    repo init

---
+++ src/Components/SchemaMenu.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
+
+const theme = ref('light')
+
+watch(theme, () => {
+  console.log(theme.value)
+  document.body.classList.value = theme.value
+})
+
+onMounted(() => {
+  console.log(theme.value)
+  document.body.classList.value = theme.value
+})
+</script>
+
+<template>
+  <header>
+    <select name="lang" id="lang">
+      <option value="pt-br">Brazilian Portuguese</option>
+      <option value="en">English</option>
+    </select>
+
+    <select name="theme" id="theme" v-model="theme">
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </header>
+</template>
+
+<style src="../assets/styles/schemaMenu.css"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SchemaMenu from '@/Components/SchemaMenu.vue'
+</script>
+
+<template>
+  <SchemaMenu />
+</template>
+
+<style scoped></style>
