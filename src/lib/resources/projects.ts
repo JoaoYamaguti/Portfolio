c47c0408e90721aa05caa60a55f822b5ecbe026c
JoaoYamaguti <joaov.yamaguti@gmail.com>
2025-11-11 23:03:06 -0300

    fix responsive layout od projects component

--- src/lib/resources/projects.ts
+++ src/lib/resources/projects.ts
@@ -1,8 +1,22 @@
 export const projects = [
   {
     name: 'Project 1',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas asperiores quis utcommodi perspiciatis assumenda optio magnam deserunt enim qui necessitatibusreprehenderit iusto exercitationem fugiat libero sapiente, magni soluta? Consectetur?',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    backgroundImage: 'icons/brazilFlag.png',
+  },
+  {
+    name: 'Project 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    backgroundImage: 'icons/brazilFlag.png',
+  },
+  {
+    name: 'Project 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    backgroundImage: 'icons/brazilFlag.png',
+  },
+  {
+    name: 'Project 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
     backgroundImage: 'icons/brazilFlag.png',
   },
 ]
