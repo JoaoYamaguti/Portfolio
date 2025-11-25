4b9681c76c502fd2ee8577cf98f139d6a7b74284
JoaoYamaguti <joaov.yamaguti@gmail.com>
2025-11-24 22:42:58 -0300

    style projectsForDesktop

--- src/Components/ProjectsComponent.vue
+++ src/Components/ProjectsComponent.vue
@@ -14,17 +14,34 @@ const content = inject<IContent>('content')
         v-for="(project, index) in content?.projects.list"
         :key="index"
         :style="{ 'background-image': 'url(' + project.backgroundImage + ')' }"
+        class="project"
       >
         <a :href="project.link" target="_blank">
           <div class="slider">
             <div class="description">
               <div class="title">
                 <h3>{{ project.name }}</h3>
-                <span :class="project.status == 'Finished' ? 'finished' : 'inProgress'">{{
-                  project.status
-                }}</span>
+                <span :class="project.status == 'Finished' ? 'finished' : 'inProgress'">
+                  {{ project.status }}
+                </span>
               </div>
               <p>{{ project.description }}</p>
+              <div class="technologies">
+                <ul class="technologiesList">
+                  <li v-for="(technology, index) in project.technologies" :key="index">
+                    <h4>{{ technology.title }}</h4>
+                    <ul class="icons">
+                      <li v-for="(icon, index) in technology.icons" :key="index">
+                        <img :src="icon" alt="" loading="lazy" />
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <!-- <div class="links">
+                <a :href="project.link">{{ content?.projects.linkLabel }}</a>
+                <a :href="project.repository">{{ content?.projects.repositoryLabel }}</a>
+              </div> -->
             </div>
           </div>
         </a>
@@ -32,7 +49,7 @@ const content = inject<IContent>('content')
     </ul>
 
     <ul class="projectsForMobile">
-      <li v-for="(project, index) in content?.projects.list" :key="index">
+      <li v-for="(project, index) in content?.projects.list" :key="index" class="project">
         <div
           class="image"
           :style="{ 'background-image': 'url(' + project.backgroundImage + ')' }"
